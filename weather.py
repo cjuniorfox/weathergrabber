8c2397163ec87827541e67275d0dd1d174686c8d
Junior_FOX <cjuniorfox@gmail.com>
2024-12-08 12:20:14 -0300

    final version

--- weather.py
+++ weather.py
@@ -2,6 +2,24 @@ from typing import List, Dict
 import json, os
 from pyquery import PyQuery as pq
 from urllib.error import HTTPError
+import argparse
+
+skycodes = {
+    32: 'sunnyDay',
+    34: 'sunnyDay',
+    31: 'clearNight',
+    26: 'cloudyFoggyDay',
+    30: 'cloudyFoggyDay' ,
+    27: 'cloudyFoggyNight',
+    11: 'rainyDay',
+    39: 'rainyDay' ,
+    12: 'rainyNight',
+    16: 'snowyIcyDay',
+    14: 'snowyIcyNight',
+    3:  'severe',
+    4:  'severe',
+    44: 'default'
+}
 
 weather_icons_fa = {
     'sunnyDay': '\uf185',        # FA Sun
@@ -13,21 +31,31 @@ weather_icons_fa = {
     'snowyIcyDay': '\uf2dc',     # FA Snowflake
     'snowyIcyNight': '\uf2dc',   # FA Snowflake (reuse)
     'severe': '\uf76c',          # FA Cloud-Showers-Heavy
-    'default': '\uf0c2'          # FA Cloud
+    'default': '\uf0c2',         # FA Cloud
+    'feel' : '\uf2c9',           # FA Thermometer
+    'wind' : '\uf72e',           # FA Wind
+    'visibility' : '\uf06e',     # FA Eye
+    'humidity' : '\uf043',       # FA Humidity
+    'rain' :  '\uf0e9'           # FA Weather Light raining
 }
 
 # Define weather emojis
 weather_icons_emoji = {
-    'sunnyDay': ' ',
+    'sunnyDay': '\u2600\ufe0f',
     'clearNight': '🌙',
     'cloudyFoggyDay': '⛅',
     'cloudyFoggyNight': '🌥️',
     'rainyDay': '🌧️',
     'rainyNight': '🌧️',
-    'snowyIcyDay': ' ',
+    'snowyIcyDay': '❄️',
     'snowyIcyNight': ' ',
     'severe': '🌩️',
-    'default': ' '
+    'default': '☁️',
+    'feel': '️️🌡️',
+    'wind': '🍃',
+    'visibility': '👁️',
+    'humidity': '💧',
+    'rain': '☔'
 }
 
 class WeatherForecast:
@@ -37,6 +65,7 @@ class WeatherForecast:
                 moment: str, 
                 status: str,
                 skycode : str,
+                icon : str,
                 chance_of_rain: str, 
                 temperature: str = None, 
                 min: str = None, 
@@ -45,6 +74,7 @@ class WeatherForecast:
             self.moment = moment
             self.status = status
             self.skycode = skycode
+            self.icon = icon
             self.chance_of_rain = chance_of_rain
             self.temperature = temperature
             self.min = min
@@ -74,9 +104,9 @@ class WeatherForecast:
         humidity: str,
         visibility: str,
         air_quality: str,
-        temperature : Dict = None,
-        hourly_predictions: List[Dict] = None,
-        daily_predictions: List[Dict] = None
+        temperature : Temperature = None,
+        hourly_predictions: List[Prediction] = None,
+        daily_predictions: List[Prediction] = None
     ):
         self.weather_id = weather_id
         self.lang = lang
@@ -88,7 +118,7 @@ class WeatherForecast:
         self.humidity = humidity
         self.visibility = visibility
         self.air_quality = air_quality
-        self.temperature = WeatherForecast.Temperature(**temperature),
+        self.temperature = WeatherForecast.Temperature(**temperature)
         self.hourly_predictions = [
             WeatherForecast.Prediction(**hourly_prediction) for hourly_prediction in (hourly_predictions or [])
         ]
@@ -130,12 +160,13 @@ class WeatherForecastExtractor:
         return status
     
     def status_code(self):
-        return self.html_data("#regionHeader").attr("class").split(" ")[2].split("-")[2]
+        self.__status_code = self.html_data("#regionHeader").attr("class").split(" ")[2].split("-")[2]
+        return self.__status_code
     
     def icon(self):
-        status_code = self.status_code()
+        status_code = self.__status_code
         return (
-            weather_icons_emoji[status_code] if status_code in weather_icons_emoji else weather_icons_emoji["default"]
+            weather_icons[status_code] if status_code in weather_icons else weather_icons["default"]
         )
 
     def wind_speed(self):
@@ -157,7 +188,8 @@ class WeatherForecastExtractor:
                 'temperature': pq(span)("span[data-testid='TemperatureValue']").text(),
                 'status' : pq(span)("svg[data-testid='Icon'] title").contents()[0],
                 'skycode' :  pq(span)("svg[data-testid='Icon']").attr('skycode'),
-                'chance_of_rain' : pq(span)("div[data-testid='SegmentPrecipPercentage'] > span").contents()[-1],
+                'icon' : self.__icon_predictions(int(pq(span)("svg[data-testid='Icon']").attr('skycode'))),
+                'chance_of_rain' : pq(span)("div[data-testid='SegmentPrecipPercentage'] > span").contents()[1],
             }
                 for span in self.html_data("section[data-testid='HourlyWeatherModule'] ul[data-testid='WeatherTable'] li")
             ]
@@ -171,7 +203,8 @@ class WeatherForecastExtractor:
                 'min': pq(span)("div[data-testid='SegmentHighTemp'] span[data-testid='TemperatureValue']").eq(1).text(),
                 'status' : pq(span)("svg[data-testid='Icon'] title").contents()[0],
                 'skycode' :  pq(span)("svg[data-testid='Icon']").attr('skycode'),
-                'chance_of_rain' : pq(span)("div[data-testid='SegmentPrecipPercentage'] span").contents()[-1],
+                'icon' : self.__icon_predictions(int(pq(span)("svg[data-testid='Icon']").attr('skycode'))),
+                'chance_of_rain' : pq(span)("div[data-testid='SegmentPrecipPercentage'] span").contents()[1],
             }
                 for span in self.html_data("section[data-testid='DailyWeatherModule'] ul[data-testid='WeatherTable'] li")
             ]
@@ -199,6 +232,10 @@ class WeatherForecastExtractor:
             daily_predictions=self.daily_predictions()  
         )
 
+    def __icon_predictions(self,skycode: int) -> str:
+        status_code = skycodes[skycode] if skycode in skycodes else 'default'
+        return weather_icons[status_code]
+
 
 def serializer(obj):
     if hasattr(obj, "__dict__"):
@@ -218,15 +255,60 @@ def grab_weather_data(lang, weather_id = None) -> str :
     except HTTPError as e:
         if(e.code == 404):
             raise ValueError(f"HTTP error {e.code} when fetching weather data. Check for the location_id or lang data.")
-    
 
 def get_weather_forecast(lang, weather_id = None) -> WeatherForecast:
     html_data = grab_weather_data(lang, weather_id)
     forecast = WeatherForecastExtractor(html_data) 
     return forecast.to_weather_forecast(weather_id,lang)
 
-## Get current locale, or use the default one
-lang = os.getenv("LANG","en_IL.UTF-8").split(".")[0].replace("_","-")
+def weather_to_waybar(wf: WeatherForecast) -> Dict:
+    hourly_predictions = "\n".join(
+        f"{h.moment}\t{'\t' if len(h.moment) < 4 else ''}  {h.temperature}\t\t{h.icon}\t{weather_icons['rain']} {h.chance_of_rain}"
+        for h in wf.hourly_predictions
+    )
+    daily_predictions = "\n".join(
+        f"{h.moment}\t{'\t' if len(h.moment) < 5 else ''}{h.max}/<small>{h.min}</small>\t{h.icon}\t{weather_icons['rain']} {h.chance_of_rain}"
+        for h in wf.daily_predictions
+    )
+    
+    tooltip_text = f"""{wf.location}
 
+<span size="xx-large">{wf.icon}\t\t{wf.temperature.current}</span>
 
-print(json.dumps(get_weather_forecast(lang=lang,weather_id='2bf40a7aa6288f1090ab35632ce451a0014b11f0b4c370b8cd4d9a018d880a3a'),default=serializer, indent=2))
+{wf.status}
+{wf.temperature.max}/<small>{wf.temperature.max}</small>\t {weather_icons['feel']} {wf.temperature.feel}
+
+{weather_icons['wind']} {wf.wind_speed}\t{weather_icons['humidity']} {wf.humidity}
+{weather_icons['visibility']} {wf.visibility}\t AQI {wf.air_quality}
+
+{hourly_predictions}
+
+{daily_predictions}
+"""
+
+    return {
+        "text": f"{wf.icon} {wf.temperature.current}",
+        "alt": wf.status,
+        "tooltip": tooltip_text,
+        "class": wf.status_code,
+    }
+
+if __name__ == "__main__":
+    ## Get current locale, or use the default one
+    parser = argparse.ArgumentParser(description="Weather forecast grabber from weather.com")
+    parser.add_argument("--location", "-l", type=str, help="64-character-hex code for location obtained from weather.com")
+    parser.add_argument("--lang", "-L", type=str, help="Language (pt-BR, fr-FR, etc.), If not set, uses the machine one.")
+    parser.add_argument("--output", "-o", type=str, choices=['json','waybar'], default='json', help="Output format. json or waybar")
+    parser.add_argument("--icons", "-i", type=str, choices=['fa','emoji'], default='emoji', help="Icon set. 'fa' for Font-Awesome, or 'widget'")
+
+    args = parser.parse_args()
+
+    weather_icons = weather_icons_emoji if args.icons == 'emoji' else weather_icons_fa
+    lang = args.lang if args.lang else os.getenv("LANG","en_IL.UTF-8").split(".")[0].replace("_","-")
+    weather_forecast = get_weather_forecast(lang=lang, weather_id=args.location)
+
+    if args.output == 'waybar':
+        waybar_data = weather_to_waybar(weather_forecast)
+        print(json.dumps(waybar_data))
+    else:
+        print(json.dumps(weather_forecast,default=serializer, indent=2))
