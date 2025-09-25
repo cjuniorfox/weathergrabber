f4d1ab389c5afd8745a4f202302324ba633f52e3
Junior_FOX <cjuniorfox@gmail.com>
2025-09-24 23:19:07 -0300

    removed no longer used file

--- weathergrabber/usecase/json_uc.py
+++
@@ -1,8 +0,0 @@
-from weathergrabber.domain.adapter.params import Params
-
-class JsonUC:
-    def __init__(self):
-        pass
-
-    def execute(self, params: Params) -> None:
-        print(f"Executing JSON use case with output format JSON")
--- weathergrabber/usecase/waybar_uc.py
+++
@@ -1,8 +0,0 @@
-from weathergrabber.domain.adapter.params import Params
-
-class WaybarUC:
-    def __init__(self):
-        pass
-
-    def execute(self, params: Params) -> None:
-        print(f"Executing Waybar use case with output format waybar")
